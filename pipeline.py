59ab10fb7454c952887ee185578c99f823f8429a
nicolasdickenmann <ndickenmann@student.ethz.ch>
2025-05-17 13:19:52 +0000

    feat: streaming of the rerun viewer, uncomment the hand tracker if required

--- pipeline.py
+++ pipeline.py
@@ -19,20 +19,20 @@ from red_loader import R3D_loader
 
 @hydra.main(config_path="conf", config_name="config", version_base=None)
 def main(cfg: DictConfig):
-    rr.init("video_stream", spawn=True)  # spawn=True ⇒ open viewer
-    rr.log("world", rr.ViewCoordinates.RIGHT_HAND_Y_UP, static=True)
+    rr.init("unique12839")  # spawn=True ⇒ open viewer
+    stream = rr.RecordingStream("my_app324324")
 
-    rr.serve_web_viewer(open_browser=False)
-
-    #ts = datetime.datetime.now().strftime("%Y%m%d_%H%M%S")
-    #rec_path = os.path.join("/workspace", f"video_stream_{ts}.rrd")
-    #rr.save(rec_path)                     # write to disk while logging 🡅
+    stream.serve_web(open_browser=True, web_port=9090, grpc_port=9876)
+    
+    stream.log("world", rr.ViewCoordinates.RIGHT_HAND_Y_UP, static=True)
 
     video_loader = R3D_loader(cfg.video)
+    #video_loader = VideoLoader(cfg.video.path)
+
 
     # hand detection
-    hand_detection = HandDetection(cfg.hand_detection_hamer)
-    # hand_detection = YoloHandDetection(cfg.hand_detection_yolo)
+    #hand_detection = HandDetection(cfg.hand_detection_hamer)
+    #hand_detection = YoloHandDetection(cfg.hand_detection_yolo)
 
     # segmentation
     segmentation = Segmentation(cfg.segmentation)
@@ -49,6 +49,8 @@ def main(cfg: DictConfig):
     if cfg.pipeline.record_seg:
         os.makedirs(SEGMENT_OUTPUT_DIR, exist_ok=True)
 
+    hand_data = None
+
     itr = 0
     # processing loop
     while True:
@@ -61,21 +63,22 @@ def main(cfg: DictConfig):
         last_process_ts = timestamp
 
         # Tag this log with an integer timeline for easy scrubbing
-        rr.set_time("time", duration=timestamp)
+        stream.set_time("time", duration=timestamp)
 
         # Optionally downscale if needed:
         # img = cv2.resize(frame_rgb, (frame_rgb.shape[1]//2, frame_rgb.shape[0]//2))
         img = frame_rgb
 
         # raw frame
-        rr.log("raw_video/frame", rr.Image(img).compress(jpeg_quality=85))
+        stream.log("raw_video/frame", rr.Image(img).compress(jpeg_quality=85))
 
         # Hand detection
+        '''
         hand_data, hd_img = hand_detection.detect_hands(
             img, timestamp_ms=int(timestamp * 1000)
         )
-        rr.log("hand_detection/annotated_image", rr.Image(hd_img))
-
+        stream.log("hand_detection/annotated_image", rr.Image(hd_img).compress(jpeg_quality=85))
+        '''
         t0 = time.perf_counter()
 
         # Segmentation
@@ -86,7 +89,7 @@ def main(cfg: DictConfig):
         t1 = time.perf_counter()
 
         print(f"Segmentation took {t1 - t0:.3f} seconds")
-        rr.log("segmentation/annotated_image", rr.Image(seg_img))
+        stream.log("segmentation/annotated_image", rr.Image(seg_img).compress(jpeg_quality=85))
 
         # ───── Save segmented frame ─────
         if cfg.pipeline.record_seg:
@@ -94,14 +97,14 @@ def main(cfg: DictConfig):
             cv2.imwrite(seg_filename, seg_img, [cv2.IMWRITE_JPEG_QUALITY, 60])
 
         # point cloud
-        rr.log("depth_map", rr.Image(depth))
-        points, colors = video_loader.generate_pcd(hd_img, depth, pose)
-        rr.log("world/point_cloud", rr.Points3D(points, colors=colors))
+        stream.log("depth_map", rr.Image(depth).compress(jpeg_quality=85))
+        #points, colors = video_loader.generate_pcd(hd_img, depth, pose)
+        #stream.log("world/point_cloud", rr.Points3D(points, colors=colors))
 
         # Graph generation
         if hand_data is not None:
          graph, graph_img = graph_generator.generate_graph(img, masks, hand_data)
-         rr.log("graph_image", rr.Image(graph_img))
+         stream.log("graph_image", rr.Image(graph_img).compress(jpeg_quality=85))
 
 
 if __name__ == "__main__":
